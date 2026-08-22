3796c6718f8165be219b6b9627b03345d4496092
Fateme Hamdavi <Fateme.hamdavi2001@gmail.com>
2026-08-22 21:17:23 +0330

    feat: add first Python program

--- hello.py
+++ hello.py
@@ -1,6 +1,2 @@
-# this is the first comment
-spam = 1  # and this is the second comment
-          # ... and now a third!
-text = "# This is not a comment because it's inside quotes."
-print(text)
-print(spam)
+print("Hello, Python!")
+print("Day 1 of my roadmap")
