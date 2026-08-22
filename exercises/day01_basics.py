8d74a5af9e9bc60df084356d2094604a029ae5b2
Fateme Hamdavi <Fateme.hamdavi2001@gmail.com>
2026-08-22 21:10:24 +0330

    chore: initialize Python roadmap

---
+++ exercises/day01_basics.py
@@ -0,0 +1,55 @@
+#me
+name = "Nazanin"
+#age = 24
+country = "Iran"
+favorite = "Python and C++"
+print("Name:",name)
+#print("Age:",age)
+print("Country:",country)
+print("Favorite programing language:",favorite)
+#Age
+birth_year = 1380
+current_year = 1405
+#age = (current_year - birth_year)
+#print(age)
+#Get two number
+num1 = int(input("Please Enter number 1: "))
+num2 = int(input("Please Enter number 2: "))
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+#Celsius to Farenheit
+temperature = float(input("Enter temperature: "))
+print("F = ",(temperature * 1.8) + 32)
+#Circle
+Radius = float(input("Enter redius: "))
+PI = 3.14
+print("Area = ", Radius * Radius * PI)
+print("Circumference = ", 2 * Radius * PI)
+#BMI
+H = float(input("Enter Hight(m): "))
+W = float(input("Enter Width: "))
+print("BMI = ",W / (H*H))
+#again age
+Age = int(input("Enter your age: "))
+Birth_year = int(input("Enter your birth year: "))
+Current_year = int(input("Enter current year: "))
+print("Age = ", (Current_year - Birth_year))
+#again calculate
+Number1 = float(input("Number1 = "))
+Number2 = float(input("Number2 = "))
+print("Sum =",Number2 + Number1 , "Diff =",Number2 - Number1, "Mult =",Number2 * Number1, "Div =", Number2 / Number1)
+#user information
+Name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+city = input("Enter your city: ")
+Favorite = input("Enter favorite programming language: ")
+print("User information:\nName:",Name, "\nAge:",age, "\nCity:", city, "\nFavorite programming language:",Favorite)
+#Calculator
+number1 = float(input("Enter number1: "))
+number2 = float(input("Enter number2: "))
+print(number2 + number1)
+print(number2 - number1)
+print(number2 / number1)
+print(number2 * number1)
